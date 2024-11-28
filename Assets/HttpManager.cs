6f5d34e8ce5f1d0b4faf018953da99781d519f8d
Anjinhyoung <117788976+Anjinhyoung@users.noreply.github.com>
2024-11-28 15:42:39 +0900

    Http-3

--- Assets/HttpManager.cs
+++ Assets/HttpManager.cs
@@ -24,6 +24,12 @@ public class HttpInfo
 {
     public string url = "";
 
+    // Body 데이터
+    public string body = "";
+
+    // contentType 
+    public string contentType = "";
+
     // 통신 성공 후 호출되는 함수 담을 변수
     public Action<DownloadHandler> onComplete;
 }
@@ -68,28 +74,91 @@ public class HttpManager : MonoBehaviour
             yield return webRequest.SendWebRequest();
 
             // 서버에게 응답이 왔다.
-            // 만약에 결과가 정상이라면
-            if(webRequest.result == UnityWebRequest.Result.Success)
-            {
-                // 우리가 원하는 데이터를 처리
-                // print(webRequest.downloadHandler.text);
-                // File.WriteAllBytes(Application.dataPath + "/image.jpg", webRequest.downloadHandler.data);
-
-                // ================================
-
-                // 응답 온 데이터를 요청한 클래스로 보내자.
-                if(info.onComplete != null)
-                {
-                    info.onComplete(webRequest.downloadHandler);
-                }
-            }
-            // 그렇지 않다면 (Error 라면)
-            else
-            {
-                // Error의 이유를 출력 
-                Debug.LogError("Net Error :" +  webRequest.error);
-            }
+            DoneRequest(webRequest, info);
         }
-
     }
+
+    // 서버에게 내가 보내는 데이터를 생성해줘
+    public IEnumerator Post(HttpInfo info)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Post(info.url, info.body, info.contentType))
+        {
+            // 서버에 요청 보내기 (응답이 올 때 까지 기다리기, 비동기 방식)
+            yield return webRequest.SendWebRequest();
+
+            // 서버에게 응답이 왔다.
+            DoneRequest(webRequest, info);
+        }
+    }
+
+     // 파일 업로드 (form - data)
+     public IEnumerator UploadFileByFormData(HttpInfo info)
+     {
+         // info.data 에는 파일의 위치 
+         // info.data에 있는 파일을 byte 배열로 읽어오자.
+         byte[] data = File.ReadAllBytes(info.body);
+
+
+         // data를 MulitpartForm 으로 셋팅
+         List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+         formData.Add(new MultipartFormFileSection("file", data, "image.jpg", info.contentType));
+
+         using (UnityWebRequest webRequest = UnityWebRequest.Post(info.url, formData))
+         {
+             // 서버에 요청 보내기 (응답이 올 때 까지 기다리기, 비동기 방식)
+             yield return webRequest.SendWebRequest();
+
+             // 서버에게 응답이 왔다.
+             DoneRequest(webRequest, info);
+         }
+     }
+
+    // 파일 업로드 
+    public IEnumerator UploadFileByByte(HttpInfo info)
+    {
+        // info.data 에는 파일의 위치 
+        // info.data에 있는 파일을 byte 배열로 읽어오자.
+        byte[] data = File.ReadAllBytes(info.body);
+
+        using (UnityWebRequest webRequest = new UnityWebRequest(info.url, "POST"))
+        {
+            // 업로드 하는 데이터 
+            webRequest.uploadHandler = new UploadHandlerRaw(data);
+            webRequest.uploadHandler.contentType = info.contentType;
+
+            // 응답 받는 데이터 공간
+            webRequest.downloadHandler = new DownloadHandlerBuffer();
+
+            // 서버에 요청 보내기 (응답이 올 때 까지 기다리기, 비동기 방식)
+            yield return webRequest.SendWebRequest();
+
+            // 서버에게 응답이 왔다.
+            DoneRequest(webRequest, info);
+        }
+    }
+
+    public IEnumerator DownloadSprtie(HttpInfo info)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequestTexture.GetTexture(info.url))
+        {
+            yield return webRequest.SendWebRequest();
+            DoneRequest(webRequest, info);
+        }
+    }
+     void DoneRequest(UnityWebRequest webRequest, HttpInfo info)
+     {
+         if (webRequest.result == UnityWebRequest.Result.Success)
+         {
+             if (info.onComplete != null)
+             {
+                 info.onComplete(webRequest.downloadHandler);
+             }
+         }
+         // 그렇지 않다면 (Error 라면)
+         else
+         {
+             // Error의 이유를 출력 
+             Debug.LogError("Net Error :" + webRequest.error);
+         }
+     }
 }
--- Assets/HttpTest.cs
+++ Assets/HttpTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class HttpTest : MonoBehaviour
 {
@@ -17,14 +18,16 @@ public class HttpTest : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             HttpInfo info = new HttpInfo();
-            info.url = "https://jsonplaceholder.typicode.com/posts";
+            info.url = "https://jsonplaceholder.typicode.com/posts=안녕하세요";
 
             info.onComplete = (DownloadHandler downloadHandler) =>
             {
-                string jsonData = "{\"data\": " + downloadHandler.text + " }"; 
-                print(jsonData);
+                print(downloadHandler);
+
+                //string jsonData = "{\"data\": " + downloadHandler.text + " }"; 
+                //print(jsonData);
                 // jsonData를 PostInfoArray 형으로 바꾸자
-                allPostInfo = JsonUtility.FromJson<PostInfoArray>(jsonData); // json 데이터를 C# 객체로 바꾸기
+                //allPostInfo = JsonUtility.FromJson<PostInfoArray>(jsonData); // json 데이터를 C# 객체로 바꾸기
             };
 
             StartCoroutine(HttpManager.GetInstance().Get(info));
@@ -37,10 +40,78 @@ public class HttpTest : MonoBehaviour
 
             info.onComplete = (DownloadHandler downloadHandler) =>
             {
-                File.WriteAllBytes(Application.dataPath + "/image2.jpg", downloadHandler.data);
+                // File.WriteAllBytes(Application.dataPath + "/image2.jpg", downloadHandler.data);
+
+             
+                // 다운로드 된 데이터를 Texture2D로 반환.
+                DownloadHandlerTexture handler = downloadHandler as DownloadHandlerTexture; 
+                Texture2D texture = handler.texture;
+
+                // texture를 이용해서 Sprite로 변환
+                Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
+
+                Image image = GameObject.Find("Image").GetComponent<Image>();
+                image.sprite = sprite;
             };
 
+            // StartCoroutine(HttpManager.GetInstance().Get(info));
+            StartCoroutine(HttpManager.GetInstance().DownloadSprtie(info));
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            // 가상의 나의 데이터를 만들자
+            UserInfo userInfo = new UserInfo();
+            userInfo.name = "메타버스";
+            userInfo.age = 3;
+            userInfo.height = 180.5f;
+
+            HttpInfo info = new HttpInfo();
+            info.url = "http://mtvs.helloworldlabs.kr:7771/api/json";
+            info.body = JsonUtility.ToJson(userInfo);
+            info.contentType = "application/json";
+            info.onComplete = (DownloadHandler downloadHandler) =>
+            {
+                print(downloadHandler.text);
+            };
             StartCoroutine(HttpManager.GetInstance().Get(info));
         }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            HttpInfo info = new HttpInfo();
+            info.url = "http://mtvs.helloworldlabs.kr:7771/api/file";
+            info.contentType = "multipart/form-data";
+            info.body = "C:\\Users\\안진형\\OneDrive\\바탕 화면\\포톤세팅.png";
+            info.onComplete = (DownloadHandler downloadHandler) =>
+            {
+                File.WriteAllBytes(Application.dataPath + "/ajh.jpg",downloadHandler.data);
+            };
+            StartCoroutine(HttpManager.GetInstance().UploadFileByFormData(info));
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            HttpInfo info = new HttpInfo();
+            info.url = "http://mtvs.helloworldlabs.kr:7771/api/byte";
+            info.contentType = "image/jpg";
+            info.body = "C:\\Users\\안진형\\OneDrive\\바탕 화면\\포톤세팅.png";
+            info.onComplete = (DownloadHandler downloadHandler) =>
+            {
+                File.WriteAllBytes(Application.dataPath + "/ajh.jpg", downloadHandler.data);
+            };
+            StartCoroutine(HttpManager.GetInstance().UploadFileByByte(info));
+        }
+
+
     }
+}
+
+[System.Serializable]
+public struct UserInfo
+{
+    public string name;
+    public int age;
+    public float height;
+
 }
